8d65022f551c654b51744a43cc409023f4e8e541
DasKashi <kashi1988@googlemail.com>
2020-11-13 01:07:50 +0100

    fixed skip_intro for de_DE
    proof: https://i.imgur.com/x3Ogr9f.jpg

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -77,7 +77,7 @@ export const i18nMap = {
   },
   de_DE: {
     next_epi: "Nächste",
-    skip_intro: "Intro überspringen",
+    skip_intro: "Vorspann überspringen",
     skip_recap: "Rückblick überspringen",
     skip_ads: "Überspringen",
   },
