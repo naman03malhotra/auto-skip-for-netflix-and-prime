b19eff7981b356fd27ebfe435f23eb791267b996
Marc <37818605+inbroso@users.noreply.github.com>
2021-07-19 12:54:40 +0200

    Update i18n.js

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -78,7 +78,8 @@ export const i18nMap = {
   de_DE: {
     next_epi: "Nächste",
     skip_intro: "Vorspann überspringen",
-    skip_recap: "Zusammenfassung überspringen",
+    skip_recap: "Rückblick überspringen",
+    skip_recap_extra: "Zusammenfassung überspringen",
     skip_ads: "Überspringen",
   },
   fi_FI: {
