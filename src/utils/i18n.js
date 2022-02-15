8391839acafd3e4db45f08b8b233563ea90f3500
Bedhator <99703885+Bedhator@users.noreply.github.com>
2022-02-15 04:07:00 +0100

    Update i18n.js

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -28,8 +28,8 @@ export const i18nMap = {
     skip_ads: "Saltar",
   },
   fr_FR: {
-    skip_intro: "Passer l'intro",
-    next_epi: "Suivant",
+    skip_intro: "Ignorer l'introduction",
+    next_epi: "Ep. suivant",
     skip_recap: "Passer le résumé",
     skip_ads: "Ignorer",
   },
