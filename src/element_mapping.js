c80e9200c76ed9155cc7c10277cdfd78b1bc2675
Naman Malhotra <naman03malhotra@gmail.com>
2024-02-28 22:47:59 +0530

    V4

--- src/element_mapping.js
+++ src/element_mapping.js
@@ -6,6 +6,8 @@ import {
   SKIP_ADS,
   NETFLIX,
   PRIME,
+  HOTSTAR,
+  JIOCINEMA
 } from "./utils/i18n";
 
 const locale = memoizedLocale("locale");
@@ -21,16 +23,21 @@ export const elementMapping = [
     selector: ".skip-credits > a",
     locale,
   },
-  {
-    type: NETFLIX,
-    selector: "[aria-label='Skip Recap']",
-    locale,
-  },
+  // {
+  //   type: NETFLIX,
+  //   selector: "[aria-label='Skip Recap']",
+  //   locale,
+  // },
   {
     type: NETFLIX,
     selector: "[aria-label='Continue Playing']",
     locale,
   },
+  // {
+  //   type: NETFLIX,
+  //   selector: "[aria-label='next-episode-seamless-button']",
+  //   locale,
+  // },
   {
     type: NETFLIX,
     selector: ".interrupter-actions > .nf-icon-button:first-child",
@@ -41,16 +48,16 @@ export const elementMapping = [
     selector: ".skipElement",
     locale,
   },
-  {
-    type: PRIME,
-    selector: ".adSkipButton",
-    locale,
-  },
-  {
-    type: PRIME,
-    selector: ".nextUpCard",
-    locale,
-  },
+  // {
+  //   type: PRIME,
+  //   selector: ".adSkipButton",
+  //   locale,
+  // },
+  // {
+  //   type: PRIME,
+  //   selector: ".nextUpCard",
+  //   locale,
+  // },
   {
     type: PRIME,
     skipEvent: SKIP_INTRO,
@@ -58,28 +65,48 @@ export const elementMapping = [
     xpath: true,
     ...translateLocale(SKIP_INTRO),
   },
+  // {
+  //   type: PRIME,
+  //   skipEvent: SKIP_RECAP,
+  //   selector: `//*[text()="${translateLocale(SKIP_RECAP).translatedText}"]`,
+  //   xpath: true,
+  //   ...translateLocale(SKIP_RECAP),
+  // },
+  // {
+  //   type: PRIME,
+  //   skipEvent: SKIP_ADS,
+  //   selector: `//*[text()="${translateLocale(SKIP_ADS).translatedText}"]`,
+  //   xpath: true,
+  //   ...translateLocale(SKIP_ADS),
+  // },
+  // {
+  //   type: PRIME,
+  //   skipEvent: NEXT_EPISODE,
+  //   selector: `//*[text()="${
+  //     translateLocale(NEXT_EPISODE).translatedText
+  //   }"]/parent::div/following-sibling::div`,
+  //   extraWait: true,
+  //   xpath: true,
+  //   ...translateLocale(NEXT_EPISODE),
+  // },
   {
-    type: PRIME,
-    skipEvent: SKIP_RECAP,
-    selector: `//*[text()="${translateLocale(SKIP_RECAP).translatedText}"]`,
+    type: HOTSTAR,
+    skipEvent: SKIP_INTRO,
+    selector: `//*[text()="${translateLocale(SKIP_INTRO).translatedText}"]`,
     xpath: true,
-    ...translateLocale(SKIP_RECAP),
+    ...translateLocale(SKIP_INTRO),
   },
+  // {
+  //   type: HOTSTAR,
+  //   skipEvent: NEXT_EPISODE,
+  //   selector: `//*[text()="${translateLocale(NEXT_EPISODE).translatedText}"]`,
+  //   xpath: true,
+  //   extraWait: true,
+  //   ...translateLocale(NEXT_EPISODE),
+  // },
   {
-    type: PRIME,
-    skipEvent: SKIP_ADS,
-    selector: `//*[text()="${translateLocale(SKIP_ADS).translatedText}"]`,
-    xpath: true,
-    ...translateLocale(SKIP_ADS),
-  },
-  {
-    type: PRIME,
-    skipEvent: NEXT_EPISODE,
-    selector: `//*[text()="${
-      translateLocale(NEXT_EPISODE).translatedText
-    }"]/parent::div/following-sibling::div`,
-    extraWait: true,
-    xpath: true,
-    ...translateLocale(NEXT_EPISODE),
-  },
+    type: JIOCINEMA,
+    skipEvent: SKIP_INTRO,
+    selector: '[aria-label="skip intro"]',
+  }
 ];
